7b16e5d21c5239f55c83466a30673df9b4cfd4af
Adam Goth <adamgoth@gmail.com>
2016-11-17 07:47:29 -0700

    Completes tutorial

--- swift-searcher/AppDelegate.swift
+++ swift-searcher/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Adam Goth. All rights reserved.
 //
 
+import CoreSpotlight
 import UIKit
 
 @UIApplicationMain
@@ -41,6 +42,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        if userActivity.activityType == CSSearchableItemActionType {
+            if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
+                if let navigationController = window?.rootViewController as? UINavigationController {
+                    if let viewController = navigationController.topViewController as? ViewController {
+                        viewController.showTutorial(Int(uniqueIdentifier)!)
+                    }
+                }
+            }
+        }
+        
+        return true
+    }
 
 }
 
